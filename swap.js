f3a68e73caad78aea25414ec3f4da150e8652520
MasumHossain71 <masumhazi311@gmail.com>
2022-11-30 01:30:36 +0600

    value swap code commited

--- swap.js
+++ swap.js
@@ -1,6 +1,22 @@
+// value swap processes
+// process-1 
 let a= 5;
 let b = 7;
 let r = a;
 a = b;
 b = r;
-console.log("After swap: a =", a, "b=", b);
+console.log("After swap: a =", a, ", b=", b);
+
+// process-2
+let x = 5;
+let y = 7;
+x = x + y;
+y = x - y;
+x = x -y;
+console.log("After swap: x =", x, ", y =", y);
+
+// process-3
+let p = 5;
+let q = 7;
+[p,q] = [q,p];
+console.log("After swap: p =", p, ", q =", q);
