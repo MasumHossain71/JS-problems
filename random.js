b2c9650081f1ab58e0f75e887b60ddb74854d181
MasumHossain71 <masumhazi311@gmail.com>
2022-11-30 01:53:22 +0600

    code added

---
+++ random.js
@@ -0,0 +1,13 @@
+// JS math object 
+let num = 2.7823323;
+let result = Math.floor(num);
+console.log(result);
+let result2 = Math.ceil(num);
+console.log(result2);
+let result3 = Math.round(num);
+console.log(result3);
+
+
+let randomNum = Math.random([1]) * 6;
+let res = Math.round(randomNum);
+console.log(res);
