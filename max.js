32521323662eb172a9a8e700f07d04460dd888eb
MasumHossain71 <masumhazi311@gmail.com>
2022-11-30 11:23:26 +0600

    find max value

---
+++ max.js
@@ -0,0 +1,16 @@
+let business = 360;
+let minister = 400;
+let amla = 656;
+if (business > minister){
+   if( business > amla){
+      console.log('Minister is Bigger.')
+   }
+}
+else if (amla > business){
+   if ( amla > minister){
+      console.log('Amla is bigger.')
+   }
+}
+else{
+   console.log('Minister is bigger.')
+}
